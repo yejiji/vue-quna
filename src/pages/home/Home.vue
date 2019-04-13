6c9168604449bcb8838f13f6c71bdf42271a1e0e
Yejj <1271361907@qq.com>
2019-04-13 23:25:18 +0800

    first commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+   <div>
+       <home-header></home-header>
+       <home-swiper></home-swiper>
+       <home-icons></home-icons>
+   </div>
+ 
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+export default {
+    name:'home',
+    components: {
+        HomeHeader,
+        HomeSwiper,
+        HomeIcons
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
+
